71de2a52e43aaeb55afe7e0ec43909a5c4ed3822
Robert <55710848+Rakancito@users.noreply.github.com>
2022-12-30 22:18:17 -0600

    Add files via upload

---
+++ root/playersettingmodule.py
@@ -0,0 +1,7 @@
+# Search
+	chrmgr.RegisterCacheEffect(chrmgr.EFFECT_PERCENT_DAMAGE3, "", "d:/ymir work/effect/hit/percent_damage3.mse")
+
+#Add after
+
+	if app.ENABLE_QUEEN_NETHIS:
+		chrmgr.RegisterCacheEffect(chrmgr.EFFECT_SNAKE_REGEN, "", "d:/ymir work/effect/monster2/snake_circle_snake.mse")
